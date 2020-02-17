4bb130129b00403f6732cb1e9310556acf581c16
kartikchaudhari <kartikchaudhari456@gmail.com>
2020-02-17 19:06:52 +0530

    initial commit

---
+++ src/java/Classes/PaytmConstants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Admin
+ */
+public class PaytmConstants {
+  public final static String MID="DIiaKw58338439656916";
+  public final static String MERCHANT_KEY="7ClcVQ7vEcbTboNE";
+  public final static String INDUSTRY_TYPE_ID="Retail";
+  public final static String CHANNEL_ID="WEB";
+  public final static String WEBSITE="WEBSTAGING";
+  public final static String PAYTM_URL="https://securegw-stage.paytm.in/theia/processTransaction"; 
+  
+  public final static String CALLBACK_URL="http://localhost:8080/paytm-jsp/pgResponse.jsp";
+}
